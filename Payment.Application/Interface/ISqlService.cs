c07693a89d45978e62aeebcfc77555920b2568fa
WINNE113 <lehuuthang12a2thptcl@gmail.com>
2023-12-05 20:48:36 +0700

    add interface to connected Db

---
+++ Payment.Application/Interface/ISqlService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment.Application.Interface
+{
+    public interface ISqlService
+    {
+        SqlParameter CreateOutputParameter(string name, SqlDbType type);
+        SqlParameter CreateOutputParameter(string name, SqlDbType type, int size);
+
+        (int, string) ExecuteNonQuery(string connectionString, string sqlObjectName, params SqlParameter[] parameters);
+
+        Task<(int, string)> ExecuteNonQueryAsyns(string connectionString, string sqlObjectName, params SqlParameter[] parameters);
+
+        (DataTable, string) FillDateTable(string connectionString, string sqlObjectName, params SqlParameter[] parameters);
+
+        Task<(DataTable, string)> FillDateTableAsyns(string connectionString, string sqlObjectName, params SqlParameter[] parameters);
+    }
+}
